6492c47100063f10df041ff3f3c5d3efda88a5e3
Chandrakant121 <csjoshi989@gmail.com>
2022-08-22 14:01:50 +0530

    Header added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,53 @@
-import { AppBar, Toolbar } from '@mui/material'
+import { AppBar, Toolbar, styled, Link, Typography } from '@mui/material'
+import { Box } from '@mui/system';
 import React from 'react'
+//typography === paragrap
+
+
+const PlusImage = styled('img')({
+    width: 10,
+    height: 10,
+    marginLeft: 4
+})
+
+
+const SubHeading = styled(Typography)`
+font-size: 10px;
+font-style: italic;
+`
+
+const Component = styled(Link)`
+    margin-left: 12%;
+    line-height: 0;
+    color: #FFFFFF;
+    text-decoration: none;
+`;
+
+const StyledHeader = styled(AppBar)`
+    background: #2874f0;
+    height: 55px;
+`;
+
 
 const Header = () => {
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
     return (
-        <AppBar>
+        <StyledHeader>
             <Toolbar>
-
+                <Component>
+                    <img src={logoURL} alt="err" style={{ width: 75 }} />
+                    <Box style={{ display: "flex" }} >
+                        <SubHeading>Explore&nbsp
+                            <Box component="span" style={{ color: '#FFE500' }}>
+                                Plus
+                            </Box>
+                        </SubHeading>
+                        <PlusImage src={subURL} />
+                    </Box>
+                </Component>
             </Toolbar>
-        </AppBar>
+        </StyledHeader>
     )
 }
 
