98468d987274910aeb6f969af7bc668f1e14094b
Chandrakant121 <csjoshi989@gmail.com>
2022-08-22 16:43:09 +0530

    Header menu updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from './components/header/Header';
 import './App.css';
+import Home from "./components/home/Home"
+import Box from '@mui/material/Box';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      <Box style={{ marginTop: 54 }} >
+        <Home />
+      </Box>
+
     </div>
   );
 }
