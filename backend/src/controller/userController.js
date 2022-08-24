8cee030b03af2796e7352fe8b450a5791028cb5c
Chandrakant121 <csjoshi989@gmail.com>
2022-08-24 20:51:57 +0530

    signup Done

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,11 +1,20 @@
 
-
-export const userSignup = (req, res) => {
+import User from "../models/user-schema.js"
+export const userSignup = async (req, res) => {
     try {
+        //====================Username check in db==================
+        const exist = await User.findOne({ username: req.body.username })
+        if (exist) {
+            return res.status(401).json({ message: "username already exist" })
+        }
+        //=====================================
         const user = req.body
+        const newUser = new User(user)
+        await newUser.save()
+        res.status(200).json({ message: user })
         // console.log(req.body)
     }
     catch (err) {
-
+        res.status(500).json({ message: err.message })
     }
 }
