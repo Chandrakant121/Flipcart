5eb9cb3e9d5c9f18fd38fd657804b5e6465f25d6
Chandrakant121 <csjoshi989@gmail.com>
2022-09-02 21:21:00 +0530

    Products fetch

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Navbar from './Navrbar'
 import Banner from './Banner'
+import { useEffect } from 'react'
 import { Box, styled } from '@mui/material'
+import { getProducts } from '../../redux/actions/productAction'
+import { useDispatch, useSelector } from 'react-redux'
 // import { Fragment } from 'react'
 // fragment are fast than div
 
@@ -11,6 +14,15 @@ background: #F2F2F2 ;
 `
 
 const Home = () => {
+    const dispatch = useDispatch()
+    const { products } = useSelector(state => state.getProducts)
+    console.log(products)
+
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [dispatch])
+
     return (
         <>
             <Navbar />
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -3,7 +3,7 @@ import { GET_PRODUCT_SUCCESS, GET_PRODUCT_FAIL } from "../../constants/productCo
 export const getProductsReducer = (state = { product: [] }, action) => {
     switch (action.type) {
         case GET_PRODUCT_SUCCESS:
-            return { producs: action.payload }
+            return { products: action.payload }
 
         case GET_PRODUCT_FAIL:
             return { err: action.payload }
