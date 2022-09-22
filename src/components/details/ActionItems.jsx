595c14ac2989c1ecfd4f9b582d804cd0563d57fa
Chandrakant121 <csjoshi989@gmail.com>
2022-09-22 11:47:43 +0530

    btn added

--- src/components/details/ActionItems.jsx
+++ src/components/details/ActionItems.jsx
@@ -1,17 +1,30 @@
 import { Box, Button, styled } from '@mui/material'
 import React from 'react'
-
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
 const LeftContainer = styled(Box)`
 min-width: 40%;
 padding: 40px 0 0 80px;
 `
 
+const Image = styled('img')({
+    padding: "15px 20px",
+    border: "1px solid #f0f0f0",
+    width: "95%"
+})
+
+const StyledButton = styled(Button)`
+width: 46%;
+height:50px;
+border-radius: 4px;
+`
+
 const ActionItems = ({ product }) => {
     return (
         <LeftContainer>
-            <img src={product.detailUrl}></img>
-            <Button variant="contained">Add to Cart</Button>
-            <Button variant="contained">Buy Now</Button>
+            <Image src={product.detailUrl} alt="product"></Image>
+            <StyledButton variant="contained" style={{ marginRight: 10, background: '#ff9f00' }}> <ShoppingCartIcon />Add to Cart</StyledButton>
+            <StyledButton variant="contained" style={{ background: "#fb541b" }}><FlashOnIcon />Buy Now</StyledButton>
         </LeftContainer>
     )
 }
--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,10 +1,23 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Grid, styled } from '@mui/material'
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { getProductDetails } from '../../redux/actions/productAction'
 import ActionItems from "./ActionItems"
+
+const Component = styled(Box)`
+ background: #F2F2F2;
+ margin-top: 55px;
+`
+const Container = styled(Grid)`
+background: #FFFFFF;
+display: flex;
+`
+
+const RightContainer = styled(Grid)`
+margin-top: 50px;
+`
 
 
 const Details = () => {
@@ -23,20 +36,36 @@ const Details = () => {
     // console.log(product)
 
     return (
-        <Box>
+        <Component>
             {
                 product && Object.keys(product).length &&
-                <Box>
-                    <Box>
-                        <ActionItems product={product} />
-                    </Box>
-                    <Box>
-                        {/* <Typography>{product.title.longTitle}</Typography> */}
-                    </Box>
+                <Container container>
 
-                </Box>
+                    <Grid item lg={4} md={4} sm={8} xs={12}>
+                        <ActionItems product={product} />
+                    </Grid>
+
+                    <RightContainer item lg={8} md={8} sm={8} xs={12}>
+                        <Typography>{product.title.longTitle}</Typography>
+
+                        <Typography style={{ marginTop: 5, color: "#878787", fontSixe: 14 }}> 9 Rating & 4 Reviews
+                            <Box component="span"><img src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png" style={{ width: 77, marginLeft: 20 }} alt="Img" /></Box>
+                        </Typography>
+
+                        <Typography>
+                            <Box component="span" style={{ fontSize: 28 }} >₹{product.price.cost}</Box>&nbsp; &nbsp;&nbsp;
+                            <Box component="span" style={{ color: "#878787" }}><strike>₹{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+                            <Box component="span" style={{ color: "#388E3C" }}>{product.price.discount}</Box>
+
+
+                        </Typography>
+
+
+                    </RightContainer>
+
+                </Container>
             }
-        </Box>
+        </Component>
     )
 }
 
